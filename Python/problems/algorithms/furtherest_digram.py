db8231e65dbc004733afb1c031f0c493c3324814
rafalencar1997 <18482768+rafalencar1997@users.noreply.github.com>
2023-02-13 14:15:27 -0300

    add furtherest digram problem

---
+++ Python/problems/algorithms/furtherest_digram.py
@@ -0,0 +1,25 @@
+
+'''
+Problem: 
+
+Given a string S, find the maximum distance
+between two equal digrams inside S
+
+Examples:
+
+input: 'akaaakakmanaa'
+output: 9
+
+input: 'aaa'
+output: 1
+
+input: 'digram'
+output: -1
+
+'''
+
+def solution(S):
+
+    # Write your code here
+    pass
+    # end of your code
